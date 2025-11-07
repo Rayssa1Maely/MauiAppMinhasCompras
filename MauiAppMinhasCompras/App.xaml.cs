305528fad7bc5b56c50878fe05a6e6154b89e2df
Rayssa Maely <rayssamaely0306@gmail.com>
2025-11-07 20:01:16 -0300

    finalizado

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -2,36 +2,35 @@
 using System.Globalization;
 
 namespace MauiAppMinhasCompras
-
-public partial class App : Application
 {
-    public partial class App : Application
-    {
-        static SQLiteDatabaseHelper _db;
-
-        public static SQLiteDatabaseHelper Db
+     public partial class App : Application
         {
-            get
+            static SQLiteDatabaseHelper _db;
+
+            public static SQLiteDatabaseHelper Db
             {
-                if(_db == null)
+                get
                 {
-                    string path = Path.Combine(
-                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "banco_sqlite_compras.db3");
+                    if (_db == null)
+                    {
+                        string path = Path.Combine(
+                            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "banco_sqlite_compras.db3");
 
-                    _db = new SQLiteDatabaseHelper(path);
+                        _db = new SQLiteDatabaseHelper(path);
+                    }
+
+                    return _db;
                 }
+            }
 
-                return _db;
+            public App()
+            {
+                InitializeComponent();
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+                //MainPage = new AppShell();
+
+                MainPage = new NavigationPage(new Views.ListaProduto());
             }
         }
-
-        public App()
-        {
-            InitializeComponent();
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
-            //MainPage = new AppShell();
-
-            MainPage = new NavigationPage(new Views.ListaProduto());
-        }
     }
-}
+
